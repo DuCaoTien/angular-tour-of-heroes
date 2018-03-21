f34087ddccab274805347dcf7ee8a29c0ef41a0b
DuCaoTien <ducaotien@gmail.com>
2018-03-21 15:08:32 +0700

    Code - updated second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,19 +8,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  person = {
-    name : "Du Cao Tien",
-    age : 22,
-    married : true,
-    wife : {
-      name : "asdsadfsdf",
-      age: 20,
-      married: true,
-    }
-  }
-
-  constructor(){
-    this.person.wife.name = "ABC";
-    this.person.wife.age = 15;
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { InputControlComponent } from './input-control/input-control.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputControlComponent  ],
+    InputControlComponent,
+    HeroesComponent],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
